87945e3554f495899afa0bbae3227db143ca9d8e
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-23 15:41:26 -0400

    workbook 4 section 2-1 exercise 1

---
+++ hotelOperations/src/test/java/com/pluralsight/hotel/operations/ReservationTest.java
@@ -0,0 +1,18 @@
+package com.pluralsight.hotel.operations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @org.junit.jupiter.api.Test
+    void setNumberOfNights() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isWeekend() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReservationTotal() {
+    }
+}
