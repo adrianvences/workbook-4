2a06eff059c9b0975e2b384e1d221c3d7a0fe4d2
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-23 15:56:13 -0400

    workbook 4 section 2-1 exercise 1 testing

--- hotelOperations/src/test/java/com/pluralsight/hotel/operations/ReservationTest.java
+++ hotelOperations/src/test/java/com/pluralsight/hotel/operations/ReservationTest.java
@@ -1,15 +1,21 @@
 package com.pluralsight.hotel.operations;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Test;
 class ReservationTest {
 
     @org.junit.jupiter.api.Test
     void setNumberOfNights() {
     }
 
+
+
     @org.junit.jupiter.api.Test
     void isWeekend() {
+
+        Reservation res = new Reservation(false,10,"king");
+        assertTrue(res.isWeekend());
+
     }
 
     @org.junit.jupiter.api.Test
